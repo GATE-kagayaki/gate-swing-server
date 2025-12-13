13930ec9953c9a5974aff5de0051b828d0eb3d29
GATE-kagayaki <hayata@cs-feel.jp>
2025-12-13 15:47:56 +0900

    Update server.py

--- server.py
+++ server.py
@@ -295,3 +295,4 @@ def api_report(report_id):
 # ==================================================
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
+
